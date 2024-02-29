f91ee5d927ab83fa26bf9b370b063e3f8fc183d1
MalvG <159794719+MalvG@users.noreply.github.com>
2024-02-29 20:20:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Briac aka Tchoupi</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Chivo:700|Overpass:400,700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header> 
+        <h1>Guillaume Malvestio</h1>
+        <p>M1 MSc ISLI</p>
+        <a href="https://www.linkedin.com/in/guillaume-malvestio-91a156204/" class="btn">MY LINKEDIN</a>
+    </header>
+
+    <div class="container">
+        <h2>Mon profil)</h2>
+        <div class="cards">
+            <div class="containercard1">
+                <div class="card">
+                    <img src="image/ludolac.jpg" alt="image1">
+                    <h3> Mes expériences professionnelles</h3>
+                </div>
+                <div class="card">
+                    <img src="image/ChevalBlanc.jpg" alt="image2">
+                    <h3>Internships</h3>
+                </div>
+            </div>
+            <div class="containercard2">
+                <div class="card">
+                    <img src="image/team.JPG" alt="image3">
+                    <h3>Associations</h3>
+                </div>
+                <div class="card">
+                    <img src="image/domicile.jpeg" alt="image4">
+                    <h3>Sporting commitments</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <div class="container">
+            <p>2024 aka le tchoupi. All rights reserved</p>
+        </div>
+    </footer>
+</body>
